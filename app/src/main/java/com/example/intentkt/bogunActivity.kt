eac5c0ab8745bb08b5fb4dd71eead1e5cd70543e
Sichan Kim <tlcks1211@naver.com>
2022-10-18 21:06:45 +0900

    android intent

---
+++ app/src/main/java/com/example/intentkt/bogunActivity.kt
@@ -0,0 +1,11 @@
+package com.example.intentkt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class bogunActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bogun)
+    }
+}
---
+++ app/src/main/java/com/example/intentkt/jungboActivity.kt
@@ -0,0 +1,11 @@
+package com.example.intentkt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class jungboActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_jungbo)
+    }
+}
